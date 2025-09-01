0646225cdf3fb071a9128ddceb211bfbed1858dc
Ash Entwisle <ash.entwisle@protonmail.com>
2025-09-01 13:22:56 +0100

    cleanup dead code
    Signed-off-by: Ash Entwisle <ash.entwisle@protonmail.com>

--- crates/wol/src/lib.rs
+++ crates/wol/src/lib.rs
@@ -2,13 +2,6 @@ use tokio::net::UdpSocket;
 
 use wotail_commons::mac_addr::MacAddr;
 
-// /// Sends a wake on lan packet to a specified mac address (broadcasting to 255.255.255.255:9)
-// pub async fn wake_on_lan(
-//     mac: &str
-// )-> Result<(), Box<dyn std::error::Error>> {
-//     wake_on_lan_broadcast(mac, "255.255.255.255:9").await // TODO: could be more efficient
-// }
-
 /// Sends a wake on lan packet to a specified mac address from a specific broadcast address
 pub async fn wake_on_lan(
     mac: &str,
